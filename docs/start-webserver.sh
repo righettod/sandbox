4c8b64613faa6aceaa66ed060a295cf5d1e467a7
Dominique RIGHETTO <righettod@users.noreply.github.com>
2022-11-13 17:30:40 +0100

    Update start-webserver.sh

--- docs/start-webserver.sh
+++ docs/start-webserver.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+function write_step(){
+    echo -e "\e[93m[+] $1\e[0m"
+}
 if [ "$#" -lt 1 ]; then
     script_name=$(basename "$0")
-    echo "Usage:"
-    echo "   $script_name [LISTENING_PORT]"
-    echo ""
-    echo "Call example:"
-    echo "    $script_name 9588"
+    write_step "Usage:"
+    echo "$script_name [LISTENING_PORT]"
+    write_step "Call example:"
+    echo "$script_name 9588"
     exit 1
 fi
 Port=$1

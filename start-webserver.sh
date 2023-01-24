27a64bccd0772c65e548c9d43acb4b8bfb2c7966
Dominique RIGHETTO <righettod@users.noreply.github.com>
2023-01-24 15:55:02 +0100

    Update start-webserver.sh

--- start-webserver.sh
+++ start-webserver.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-function write_step(){
-    echo -e "\e[93m[+] $1\e[0m"
-}
-if [ "$#" -lt 1 ]; then
-    script_name=$(basename "$0")
-    write_step "Usage:"
-    echo "$script_name [LISTEN_PORT]"
-    write_step "Call example:"
-    echo "$script_name 9588"
-    exit 1
-fi
-Port=$1
-php -S 0.0.0.0:$Port -t docs
+port=$(netstat -nutela | grep "LISTEN" | grep -Po ":[0-9]+" | tr -d ':')
+php -S 0.0.0.0:$port -t docs

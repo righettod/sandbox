4e020145dfc46c6b13b65b1fb5cac1e46a82995e
Dominique RIGHETTO <righettod@users.noreply.github.com>
2023-01-24 16:37:36 +0100

    Update start-webserver.sh

--- start-webserver.sh
+++ start-webserver.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Start a webserver from a THC disposable server
-host=$(cat /config/self/reverse_ip)
 port=$(cat /config/self/reverse_port)
-php -S $host:$port -t docs
+php -S 0.0.0.0:$port -t docs

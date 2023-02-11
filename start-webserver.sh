478d2b60f35b1924301e04c18202a979c5197d9b
drighetto <drighetto@excellium-services.com>
2023-02-11 14:21:42 +0100

    cleanup

--- start-webserver.sh
+++ start-webserver.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # Start a webserver from a THC disposable server
 port=$(cat /config/self/reverse_port)
+ip=$(cat /config/self/reverse_ip)
+echo "[i] Server url: http://$ip:$port"
 php -S 0.0.0.0:$port -t docs

65b2bafc03e259d81a65c33ea5eae709e900d3dc
drighetto <drighetto@excellium-services.com>
2023-02-24 08:35:51 +0100

    add exo

--- docs/exercices/jwt-sandbox-start.sh
+++ docs/exercices/jwt-sandbox-start.sh
@@ -4,4 +4,4 @@ ip=$(cat /config/self/reverse_ip)
 export FLASK_ENV="production"
 export FLASK_APP="jwt-sandbox"
 echo "[i] Server url: http://$ip:$port"
-flask run --port=$port --eager-loading --no-reload --no-debugger --with-threads
+flask run --host=0.0.0.0 --port=$port --eager-loading --no-reload --no-debugger --with-threads

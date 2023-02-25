d946b4ec5ff0404d2a4190ab5dd25a7448f6906c
drighetto <drighetto@excellium-services.com>
2023-02-25 10:52:14 +0100

    cleanup

--- docs/exercices/jwt-sandbox-start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-port=$(cat /config/self/reverse_port)
-ip=$(cat /config/self/reverse_ip)
-export FLASK_ENV="production"
-export FLASK_APP="jwt-sandbox"
-echo "[i] Server url: http://$ip:$port"
-flask run --host=0.0.0.0 --port=$port --eager-loading --no-reload --no-debugger --with-threads
